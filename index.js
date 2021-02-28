0ff5efb175679a7dae694508d155b258d7392817
Restyled.io <commits@restyled.io>
2021-02-28 16:18:47 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -4,111 +4,106 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http)
-const { parser, htmlOutput, toHTML } = require("discord-markdown");
+const {parser, htmlOutput, toHTML} = require("discord-markdown");
 const emoji = require("discord-emoji-converter");
 
 io.on("connection", (socket) => {
-	io.emit("message", {author: "Welcome bot", content: "Hello! Welcome to discord unblocked! Send something to any channel this bot is in!", time: (new Date).toString(), color: "00bbbb", attachments: []})
+  io.emit("message", {
+    author : "Welcome bot",
+    content :
+        "Hello! Welcome to discord unblocked! Send something to any channel this bot is in!",
+    time : (new Date).toString(),
+    color : "00bbbb",
+    attachments : []
+  })
   io.emit("channels", h)
   console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+  socket.on("disconnect", () => { console.log("user disconnected"); });
   socket.on("new_message", (message) => {
-    
-  console.log('Hi')
+    console.log('Hi')
     console.log(message);
     console.log(socket)
-  try {
-    if (message.password === process.env.PASSWORD) {
-      
-      
-      channel=client.channels.cache.get(message.c);
-      channel.send(`**${message.u}**: ${message.q}`);
-      socket.emit('success', 'Sent')
-    } else {
-      socket.emit('error', 'Invalid password')
-      console.log("Invalid password");
+    try {
+      if (message.password === process.env.PASSWORD) {
+
+        channel = client.channels.cache.get(message.c);
+        channel.send(`**${message.u}**: ${message.q}`);
+        socket.emit('success', 'Sent')
+      } else {
+        socket.emit('error', 'Invalid password')
+        console.log("Invalid password");
+      }
+    } catch (e) {
+      console.error(e.stack);
+      socket.emit('error', e.stack)
+      // res.send("Error sending message! \n\n" + e.stack);
     }
-  } catch (e) {
-    console.error(e.stack);
-    socket.emit('error', e.stack)
-    //res.send("Error sending message! \n\n" + e.stack);
-  }
-
-})
-
+  })
 });
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
+app.get("/", (req, res) => { res.sendFile(`${__dirname}/index.html`); });
 var channel;
-var h={}
-/*app.get("/send", (req, res) => {
-  console.log(req.query);
-  try {
-    if (req.query.password === process.env.PASSWORD) {
-      res.send("Sent!");
-      
-      channel=client.channels.cache.get(req.query.c);
-      channel.send(`**${req.query.u}**: ${req.query.q}`);
-    } else {
-      res.send("Invalid password!");
-      console.log("Invalid password");
-    }
-  } catch (e) {
-    console.error(e.stack);
-    res.send("Error sending message! \n\n" + e.stack);
-  }
-});*/
+var h = {}
+        /*app.get("/send", (req, res) => {
+          console.log(req.query);
+          try {
+            if (req.query.password === process.env.PASSWORD) {
+              res.send("Sent!");
 
-client.on("ready", () => {
-  channel = client.channels.cache.get("798550831292874815");
-  
-  for (let value of client.channels.cache.entries()) {
-    if (value[1].type === "text") {
-      h[value[1].guild.name + ": " + value[1].name] = value[1].id;
-    }
+              channel=client.channels.cache.get(req.query.c);
+              channel.send(`**${req.query.u}**: ${req.query.q}`);
+            } else {
+              res.send("Invalid password!");
+              console.log("Invalid password");
+            }
+          } catch (e) {
+            console.error(e.stack);
+            res.send("Error sending message! \n\n" + e.stack);
+          }
+        });*/
+
+        client.on("ready", () => {
+          channel = client.channels.cache.get("798550831292874815");
+
+          for (let value of client.channels.cache.entries()) {
+            if (value[1].type === "text") {
+              h[value[1].guild.name + ": " + value[1].name] = value[1].id;
+            }
+          }
+          console.log(h)
+          // console.log(client.channels.cache.entries().next().value[1].name)
+        });
+client.on("message", function(message) {
+  findUser = (id) => { return client.users.cache.find((u) => u.id === id); };
+  if (message.content.includes('change-nick ')) {
+    message.guild.me.setNickname(message.content.replace("change-nick  ", ""))
+    return message.channel.send('Changed!');
   }
-  console.log(h)
-  //console.log(client.channels.cache.entries().next().value[1].name)
-});
-client.on("message", function (message) {
-  findUser = (id) => {
-    return client.users.cache.find((u) => u.id === id);
-  };
-	if (message.content.includes('change-nick ')) {
-			message.guild.me.setNickname(message.content.replace("change-nick  ", ""))
-			return message.channel.send('Changed!');
-	}
   if (message.content === "ping" && !message.author.bot) {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
   }
   let attachments = message.attachments
   io.emit("message", {
-    author: message.author.tag,
-		time: (new Date).toString(),
-    content: toHTML(emoji.emojify(message.content), {
-      discordCallback: {
-        user: (node) => "<b>@" + findUser(node.id).username + "</b>",
+    author : message.author.tag,
+    time : (new Date).toString(),
+    content : toHTML(emoji.emojify(message.content), {
+      discordCallback : {
+        user : (node) => "<b>@" + findUser(node.id).username + "</b>",
       },
     }),
-    color: message.member ? message.member.displayHexColor : "#fff",
-    attachments: attachments,
-		channel: message.channel.id
+    color : message.member ? message.member.displayHexColor : "#fff",
+    attachments : attachments,
+    channel : message.channel.id
   });
   for (let embed of message.embeds) {
-	//Just `embed` is fine, go to index.html now
-  
-  io.emit("embed", embed)
-}
+    // Just `embed` is fine, go to index.html now
+
+    io.emit("embed", embed)
+  }
 
   /*app.get("/send", (req, res) => {
     channel.send(`**${req.query.u}**: req.query.q`);
   });*/
 });
 client.login(process.env.BOT_TOKEN);
-http.listen(3000, () => {
-  console.log("Exploding on port 3000");
-});
+http.listen(3000, () => { console.log("Exploding on port 3000"); });
